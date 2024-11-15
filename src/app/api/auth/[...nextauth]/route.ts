2b1f39df25a8e55fce1a582f1ca3e4ec2fc71d08
top0329 <top000329@gmail.com>
2024-11-15 19:24:57 +0900

    build: fix esLint errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,73 +1,37 @@
-import { AUTH_SECRET, JWT_EXPIRES } from "@/lib/constants";
-import NextAuth from "next-auth";
+import { AUTH_SECRET } from "@/lib/constants";
 import { prisma } from "@/lib/prisma";
-import ms from "ms";
-import jwt from "jsonwebtoken";
+import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { NextResponse } from "next/server";
 
 const authOptions = {
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      httpOptions: {
+        timeout: 10000,
+      },
     }),
   ],
   callbacks: {
     async signIn({ user }: any) {
       const { email } = user;
-      let tokenPayload;
 
-      const existingUser = await prisma.user.findFirst({
+      const auth = await prisma.user.findFirst({
         where: { email },
       });
 
-      if (!existingUser) {
-        const user = await prisma.user.create({
-          data: {
-            email,
-            registerType: "google",
-            emailVerified: true,
-          },
+      if (!auth)
+        await prisma.user.create({
+          data: { email, registerType: "google", emailVerified: true },
         });
-        tokenPayload = { user: user?.id };
-      } else tokenPayload = { user: existingUser?.id };
 
-      const res = NextResponse.json({ success: true, user }, { status: 200 });
-
-      const token = jwt.sign(tokenPayload, AUTH_SECRET, {
-        expiresIn: JWT_EXPIRES,
-      });
-
-      const expires = new Date();
-
-      expires.setMilliseconds(expires.getMilliseconds() + ms(JWT_EXPIRES));
-
-      // Set your custom cookie
-      res.cookies.set("Authentication", token, {
-        expires,
-        secure: true,
-        httpOnly: true,
-      });
-
-      return res;
+      return "/profile/invitation-code";
     },
   },
   secret: AUTH_SECRET,
 };
 
-const handler = NextAuth({
-  ...authOptions,
-  callbacks: {
-    ...authOptions.callbacks,
-    async signIn({ user }: any) {
-      const result = await authOptions.callbacks.signIn({ user });
-      if (result instanceof NextResponse) {
-        return true;
-      }
-      return result;
-    },
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
